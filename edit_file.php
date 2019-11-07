13f5cf24d8db0a8982edf005c5deb78b62127c30
xxxbbbrrr <xbr123@yahoo.com.tw>
2019-11-07 15:32:00 +0800

    檔名及路徑的另一種做法

--- edit_file.php
+++ edit_file.php
@@ -2,65 +2,74 @@
 $dsn = "mysql:host=localhost;charset=utf8;dbname=upload";
 $pdo = new PDO($dsn,"root","");
 
-if (!empty($_FILES) && $_FILES['file']['error']==0){       
-    $filename=$_FILES['file']['name'];
+if(!empty($_FILES) && $_FILES['file']['error']==0){
     $type=$_FILES['file']['type'];
+    $filename=$_FILES['file']['name'];
     $path="./upload/";
     $updateTime=date("Y-m-d H:i:s");
+    $origin_name=$_FILES['file']['name'];
+    $save_name=md5(time().$_FILES['file']['name']);
+    switch($_FILES['file']['type']){
+        case "image/jpeg":
+            $subname=".jpg";
+        break;
+        case "image/png":
+            $subname=".png";
+        break;
+        case "image/gif":
+            $subname=".gif";
+        break;
+        default:
+            $subname=".other";
+    }
+    $path="./upload/".$save_name.$subname;    
     $id=$_POST['id'];
-    move_uploaded_file($_FILES['file']['tmp_name'], $path . $filename);
-    
-    // 刪除原本的檔案
+    move_uploaded_file($_FILES['file']['tmp_name'] , $path );
+    //刪除原本的檔案
     $sql="select * from files where id='$id'";
     $origin=$pdo->query($sql)->fetch();
     $origin_file=$origin['path'];
     unlink($origin_file);
-
-    // 更新資料
-    $sql="update files set name='$filename', type='$type', update_time='$updateTime', path='" . $path . $filename . "' where id='$id'";
-
+    //更新資料
+    $sql="update files set name='$origin_name',type='$type',update_time='$updateTime',path=' $path ' where id='$id'";
     $result=$pdo->exec($sql);
-    if ($result==1){
+    if($result==1){
         echo "更新成功";
         header("location:manage.php");
     }else{
         echo "DB有誤";
     }
 }
-
 $id=$_GET['id'];
 $sql="select * from files where id='$id'";
-$date=$pdo->query($sql)->fetch();
-
+$data=$pdo->query($sql)->fetch();
 ?>
-
 <form action="edit_file.php" method="post" enctype="multipart/form-data">
-
 <table>
     <tr>
-        <td colspan="2"></td>
-        <td><img src="<?=$date['path'];?>" style="width:200px;heigth:200px"></td>
+        <td colspan="2">
+            <img src="<?=$data['path'];?>" style="width:200px;height:200px">
+        </td>
+
     </tr>
     <tr>
         <td>name</td>
-        <td><?=$date['name'];?></td>
+        <td><?=$data['name'];?></td>
     </tr>
     <tr>
         <td>path</td>
-        <td><?=$date['path'];?></td>
+        <td><?=$data['path'];?></td>
     </tr>
     <tr>
         <td>type</td>
-        <td><?=$date['type'];?></td>
+        <td><?=$data['type'];?></td>
     </tr>
     <tr>
         <td>create_time</td>
-        <td><?=$date['create_time'];?></td>
+        <td><?=$data['create_time'];?></td>
     </tr>
 </table>
-
-<input type="file" name="file"><br>
-<input type="hidden" name="id" value="<?=$date['id'];?>">
+更新檔案:<input type="file" name="file"><br>
+<input type="hidden" name="id" value="<?=$data['id'];?>">
 <input type="submit" value="更新">
-
 </form>
--- manage.php
+++ manage.php
@@ -9,83 +9,88 @@
     $dsn = "mysql:host=localhost;charset=utf8;dbname=upload";
     $pdo = new PDO($dsn,"root","");
 
-    if (!empty($_FILES) && $_FILES['file']['error']==0){
+    if(!empty($_FILES) && $_FILES['file']['error']==0){
+   
+        $type=$_FILES['file']['type'];
+        $origin_name=$_FILES['file']['name'];
+        $save_name=md5(time().$_FILES['file']['name']);
+        switch($_FILES['file']['type']){
+            case "image/jpeg":
+                $subname=".jpg";
+            break;
+            case "image/png":
+                $subname=".png";
+            break;
+            case "image/gif":
+                $subname=".gif";
+            break;
+            default:
+                $subname=".other";
+        }
+        $path="./upload/".$save_name.$subname;
         
-        $filename = $_FILES['file']['name'];
-        $type = $_FILES['file']['type'];
-        $path = "./upload/";
-    
-        move_uploaded_file($_FILES['file']['tmp_name'], $path . $filename);
-        
-        $sql = "insert into files (`name`, `type`, `path`) values ('$filename', '$type', '" . $path . $filename . "')";
-
-        $result = $pdo->exec($sql);
-        if ($result==1){
+        move_uploaded_file($_FILES['file']['tmp_name'] , $path);
+        $sql="insert into files (`name`,`type`,`path`) values('$origin_name','$type','$path')";
+        $result=$pdo->exec($sql);
+        if($result==1){
             echo "上傳成功";
         }else{
             echo "DB有誤";
         }
-
     }
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>檔案管理功能</title>
-    <link rel="stylesheet" href="style.css">
-    <style>
-
-    a{
-        display: inline-block;
-        border: 1px solid #ccc;
-        padding: 5px 15px;
-        border-radius: 20px;
-        box-shadow: 1px 1px 3px #ccc;
-    }
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>檔案管理功能</title>
+        <link rel="stylesheet" href="style.css">
+        <style>
+        a{
+            display: inline-block;
+            border: 1px solid #ccc;
+            padding: 5px 15px;
+            border-radius: 20px;
+            box-shadow: 1px 1px 3px #ccc;
+        }
+        
+        </style>
+    </head>
+    <body>
+    <h1 class="header">檔案管理練習</h1>
+    <!----建立上傳檔案表單及相關的檔案資訊存入資料表機制----->
+    <form action="manage.php" method="post" enctype="multipart/form-data">
+      檔案：<input type="file" name="file" ><br>
+      <input type="submit" value="上傳">
+    </form>
     
-    </style>
-</head>
-<body>
-<h1 class="header">檔案管理練習</h1>
-<!----建立上傳檔案表單及相關的檔案資訊存入資料表機制----->
-
-<form action="manage.php" method="post" enctype="multipart/form-data">
-  檔案：<input type="file" name="file" ><br>
-  <input type="submit" value="上傳">
-</form>
-
-
-
-<!----透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作----->
-
-<table>
-    <tr>
-        <td>id</td>
-        <td>name</td>
-        <td>type</td>
-        <td>path</td>
-        <td>create time</td>
-        <td>操作</td>
-    </tr>
-
-<?php
-
-    $sql = "select * from files";
-    $rows = $pdo -> query($sql) -> fetchAll();
-    foreach ($rows as $key => $file){
-
-?>
-
-    <tr>
-        <td><?=$file['id'];?></td>
-        <td><?=$file['name'];?></td>
-        <td><?=$file['type'];?></td>
-        <td><?=$file['path'];?></td>
-        <td><?=$file['create_time'];?></td>
+    <!----透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作----->
+    
+    <table>
+        <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>type</td>
+            <td>縮圖</td>
+            <td>path</td>
+            <td>create time</td>
+            <td>操作</td>
+        </tr>
+    
+        <?php
+            $sql="select * from files";
+            $rows=$pdo->query($sql)->fetchAll();
+            foreach ($rows as $key => $file) {  
+        ?>
+        <tr>
+            <td><?=$file['id'];?></td>
+            <td><?=$file['name'];?></td>
+            <td><?=$file['type'];?></td>
+            <td><img src="<?=$file['path'];?>" style="width:100px ;height:50px;"></td>
+            <td><?=$file['path'];?></td>
+            <td><?=$file['create_time'];?></td>
         <td>
         <a href="edit_file.php?id=<?=$file['id'];?>">更新檔案</a>
         <a href="del_file.php?id=<?=$file['id'];?>">刪除檔案</a>
