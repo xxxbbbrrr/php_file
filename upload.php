3f15cb36a31fde3325c6841040ca7eb06d28b5b5
xxxbbbrrr <xbr123@yahoo.com.tw>
2019-11-07 09:33:52 +0800

    表單檔案上傳功能

--- upload.php
+++ upload.php
@@ -6,6 +6,21 @@
  * 4.顯示檔案列表
  */
 
+// if (!empty($_FILES && $_FILES['img']['error']==0)){
+if(!empty($_POST)){
+    echo "有POST的資料<br>";
+    echo $_FILES['img']['error'];
+    echo "<br>";
+    echo $_FILES['img']['tmp_name'];
+    echo "<br>";
+    echo $_FILES['img']['type'];
+    echo "<br>";
+    echo $_FILES['img']['size'];
+
+    move_uploaded_file($_FILES['img']['tmp_name'],"./img/".$_FILES['img']['name']);
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,11 +35,17 @@
  <h1 class="header">檔案上傳練習</h1>
  <!----建立你的表單及設定編碼----->
 
-
-
+ <form action="upload.php" method="post" enctype="multipart/form-data">
+  檔案：<input type="file" name="img" ><br>
+  檔案說明：<input type="text" name="imgdesc"><br>
+  <input type="submit" value="上傳">
+</form>
 
 
 <!----建立一個連結來查看上傳後的圖檔---->  
+
+<br><br>
+<a href="./img/">查看上傳的檔案</a>
 
 
 </body>
